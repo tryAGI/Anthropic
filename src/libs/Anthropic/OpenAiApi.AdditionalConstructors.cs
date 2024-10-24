c3bed7c821b7fb72e5e18a84127612b04804808c
HavenDV <havendv@gmail.com>
2024-10-24 08:09:37 +0400

    feat: Added new anthropic-beta headers.

--- src/libs/Anthropic/OpenAiApi.AdditionalConstructors.cs
+++ src/libs/Anthropic/OpenAiApi.AdditionalConstructors.cs
@@ -8,7 +8,13 @@ public partial class AnthropicApi
     partial void Initialized(HttpClient client)
     {
         client.DefaultRequestHeaders.Add("anthropic-version", "2023-06-01");
-        client.DefaultRequestHeaders.Add("anthropic-beta", "tools-2024-04-04"); //max-tokens-3-5-sonnet-2024-07-15
+        client.DefaultRequestHeaders.Add("anthropic-beta", [
+            "tools-2024-04-04",
+            "message-batches-2024-09-24",
+            "prompt-caching-2024-07-31",
+            "computer-use-2024-10-22",
+            //"max-tokens-3-5-sonnet-2024-07-15",
+        ]);
         client.DefaultRequestHeaders.Add("User-Agent", "tryAGI/Anthropic");
         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
     }
