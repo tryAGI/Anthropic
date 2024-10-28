896715edbc046d6ccb94c3ea2e151752122c78ab
Stephen Toub <stoub@microsoft.com>
2024-10-28 16:11:04 -0400

    feat: Add IChatClient implementation to AnthropicApi (#71)

--- src/libs/Anthropic/JsonSerializerContextTypes.AdditionalTypes.cs
+++ src/libs/Anthropic/JsonSerializerContextTypes.AdditionalTypes.cs
@@ -1,4 +1,8 @@
 using CSharpToJsonSchema;
+using System.Text.Json.Serialization;
+
+#pragma warning disable CA1002 // Do not expose generic lists
+#pragma warning disable CA2227 // Collection properties should be read only
 
 namespace Anthropic;
 
@@ -8,14 +12,48 @@ public partial class JsonSerializerContextTypes
     /// 
     /// </summary>
     public OpenApiSchema? OpenApiSchema { get; set; }
-    
+
     /// <summary>
     /// 
     /// </summary>
     public MessageStreamEvent? MessageStreamEvent { get; set; }
-    
+
     /// <summary>
     /// 
     /// </summary>
     public JsonElement? JsonElement { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public Dictionary<string, object?>? DictionaryStringObject { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public ToolParameterJsonSchema? ToolParameterJsonSchema { get; set; }
+}
+
+/// <summary>
+/// 
+/// </summary>
+public sealed class ToolParameterJsonSchema
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [JsonPropertyName("type")]
+    public string Type { get; set; } = "object";
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [JsonPropertyName("required")]
+    public List<string> Required { get; set; } = [];
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [JsonPropertyName("properties")]
+    public Dictionary<string, JsonElement> Properties { get; set; } = [];
 }
