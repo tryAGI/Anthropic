75e027790942ab6d26b5e096d48648ca5a3fc995
Konstantin S. <havendv@gmail.com>
2026-04-02 08:55:45 +0400

    feat: Updated OpenAPI spec (#238)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Anthropic/Generated/Anthropic.Models.AnthropicBeta.g.cs
+++ src/libs/Anthropic/Generated/Anthropic.Models.AnthropicBeta.g.cs
@@ -13,35 +13,35 @@ namespace Anthropic
         /// 
         /// </summary>
 #if NET6_0_OR_GREATER
-        public string? Value1 { get; init; }
+        public string? AnthropicBetaVariant1 { get; init; }
 #else
-        public string? Value1 { get; }
+        public string? AnthropicBetaVariant1 { get; }
 #endif
 
         /// <summary>
         /// 
         /// </summary>
 #if NET6_0_OR_GREATER
-        [global::System.Diagnostics.CodeAnalysis.MemberNotNullWhen(true, nameof(Value1))]
+        [global::System.Diagnostics.CodeAnalysis.MemberNotNullWhen(true, nameof(AnthropicBetaVariant1))]
 #endif
-        public bool IsValue1 => Value1 != null;
+        public bool IsAnthropicBetaVariant1 => AnthropicBetaVariant1 != null;
 
         /// <summary>
         /// 
         /// </summary>
 #if NET6_0_OR_GREATER
-        public global::Anthropic.AnthropicBetaEnum? Value2 { get; init; }
+        public global::Anthropic.AnthropicBetaEnum? Enum { get; init; }
 #else
-        public global::Anthropic.AnthropicBetaEnum? Value2 { get; }
+        public global::Anthropic.AnthropicBetaEnum? Enum { get; }
 #endif
 
         /// <summary>
         /// 
         /// </summary>
 #if NET6_0_OR_GREATER
-        [global::System.Diagnostics.CodeAnalysis.MemberNotNullWhen(true, nameof(Value2))]
+        [global::System.Diagnostics.CodeAnalysis.MemberNotNullWhen(true, nameof(Enum))]
 #endif
-        public bool IsValue2 => Value2 != null;
+        public bool IsEnum => Enum != null;
         /// <summary>
         /// 
         /// </summary>
@@ -50,14 +50,14 @@ namespace Anthropic
         /// <summary>
         /// 
         /// </summary>
-        public static implicit operator string?(AnthropicBeta @this) => @this.Value1;
+        public static implicit operator string?(AnthropicBeta @this) => @this.AnthropicBetaVariant1;
 
         /// <summary>
         /// 
         /// </summary>
         public AnthropicBeta(string? value)
         {
-            Value1 = value;
+            AnthropicBetaVariant1 = value;
         }
 
         /// <summary>
@@ -68,42 +68,42 @@ namespace Anthropic
         /// <summary>
         /// 
         /// </summary>
-        public static implicit operator global::Anthropic.AnthropicBetaEnum?(AnthropicBeta @this) => @this.Value2;
+        public static implicit operator global::Anthropic.AnthropicBetaEnum?(AnthropicBeta @this) => @this.Enum;
 
         /// <summary>
         /// 
         /// </summary>
         public AnthropicBeta(global::Anthropic.AnthropicBetaEnum? value)
         {
-            Value2 = value;
+            Enum = value;
         }
 
         /// <summary>
         /// 
         /// </summary>
         public AnthropicBeta(
-            string? value1,
-            global::Anthropic.AnthropicBetaEnum? value2
+            string? anthropicBetaVariant1,
+            global::Anthropic.AnthropicBetaEnum? @enum
             )
         {
-            Value1 = value1;
-            Value2 = value2;
+            AnthropicBetaVariant1 = anthropicBetaVariant1;
+            Enum = @enum;
         }
 
         /// <summary>
         /// 
         /// </summary>
         public object? Object =>
-            Value2 as object ??
-            Value1 as object 
+            Enum as object ??
+            AnthropicBetaVariant1 as object 
             ;
 
         /// <summary>
         /// 
         /// </summary>
         public override string? ToString() =>
-            Value1?.ToString() ??
-            Value2?.ToValueString() 
+            AnthropicBetaVariant1?.ToString() ??
+            Enum?.ToValueString() 
             ;
 
         /// <summary>
@@ -111,15 +111,15 @@ namespace Anthropic
         /// </summary>
         public bool Validate()
         {
-            return IsValue1 || IsValue2;
+            return IsAnthropicBetaVariant1 || IsEnum;
         }
 
         /// <summary>
         /// 
         /// </summary>
         public TResult? Match<TResult>(
-            global::System.Func<string?, TResult>? value1 = null,
-            global::System.Func<global::Anthropic.AnthropicBetaEnum?, TResult>? value2 = null,
+            global::System.Func<string?, TResult>? anthropicBetaVariant1 = null,
+            global::System.Func<global::Anthropic.AnthropicBetaEnum?, TResult>? @enum = null,
             bool validate = true)
         {
             if (validate)
@@ -127,13 +127,13 @@ namespace Anthropic
                 Validate();
             }
 
-            if (IsValue1 && value1 != null)
+            if (IsAnthropicBetaVariant1 && anthropicBetaVariant1 != null)
             {
-                return value1(Value1!);
+                return anthropicBetaVariant1(AnthropicBetaVariant1!);
             }
-            else if (IsValue2 && value2 != null)
+            else if (IsEnum && @enum != null)
             {
-                return value2(Value2!);
+                return @enum(Enum!);
             }
 
             return default(TResult);
@@ -143,8 +143,8 @@ namespace Anthropic
         /// 
         /// </summary>
         public void Match(
-            global::System.Action<string?>? value1 = null,
-            global::System.Action<global::Anthropic.AnthropicBetaEnum?>? value2 = null,
+            global::System.Action<string?>? anthropicBetaVariant1 = null,
+            global::System.Action<global::Anthropic.AnthropicBetaEnum?>? @enum = null,
             bool validate = true)
         {
             if (validate)
@@ -152,13 +152,13 @@ namespace Anthropic
                 Validate();
             }
 
-            if (IsValue1)
+            if (IsAnthropicBetaVariant1)
             {
-                value1?.Invoke(Value1!);
+                anthropicBetaVariant1?.Invoke(AnthropicBetaVariant1!);
             }
-            else if (IsValue2)
+            else if (IsEnum)
             {
-                value2?.Invoke(Value2!);
+                @enum?.Invoke(Enum!);
             }
         }
 
@@ -169,9 +169,9 @@ namespace Anthropic
         {
             var fields = new object?[]
             {
-                Value1,
+                AnthropicBetaVariant1,
                 typeof(string),
-                Value2,
+                Enum,
                 typeof(global::Anthropic.AnthropicBetaEnum),
             };
             const int offset = unchecked((int)2166136261);
@@ -189,8 +189,8 @@ namespace Anthropic
         public bool Equals(AnthropicBeta other)
         {
             return
-                global::System.Collections.Generic.EqualityComparer<string?>.Default.Equals(Value1, other.Value1) &&
-                global::System.Collections.Generic.EqualityComparer<global::Anthropic.AnthropicBetaEnum?>.Default.Equals(Value2, other.Value2) 
+                global::System.Collections.Generic.EqualityComparer<string?>.Default.Equals(AnthropicBetaVariant1, other.AnthropicBetaVariant1) &&
+                global::System.Collections.Generic.EqualityComparer<global::Anthropic.AnthropicBetaEnum?>.Default.Equals(Enum, other.Enum) 
                 ;
         }
 
